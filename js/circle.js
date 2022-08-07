ebbb7121faf871ce3e52425519b8395e0ef85ef2
pauldubois98 <pauldubois98@gmail.com>
2022-08-08 01:36:25 +0200

    calc centers correctly

--- js/circle.js
+++ js/circle.js
@@ -1,12 +1,19 @@
 var images_div = document.getElementById("faces");
+var hidden_images_div = document.getElementById("hidden_faces");
 var images = [];
+var hidden_images = [];
 var images_landmarks = [];
+var images_centers = [];
 
 function addImage(file) {
     const img = document.createElement("img");
     img.src = URL.createObjectURL(file);
     images_div.appendChild(img);
     images.push(img);
+    const hidden_img = document.createElement("img");
+    hidden_img.src = URL.createObjectURL(file);
+    hidden_images_div.appendChild(hidden_img);
+    hidden_images.push(hidden_img);
 }
 
 function upload(event) {
@@ -17,32 +24,34 @@ function upload(event) {
     circle();
 }
 async function calculate_landmarks() {
-    for (let index = 0; index < images.length; index++) {
-        const img = images[index];
+    for (let index = 0; index < hidden_images.length; index++) {
+        const img = hidden_images[index];
         landmarks = await get_landmarks(img);
         images_landmarks.push(landmarks);
-        var c = center(images_landmarks[index]._positions)
-        console.log(c)
-        console.log(index)
     }
     console.log(images_landmarks);
+    calculate_centers();
+    console.log(images_centers);
 }
 
-function center(array) {
-    var total_x = 0;
-    var total_y = 0;
-    var count = 0;
-
-    array.forEach(function(item, index) {
-        total_x += item._x;
-        total_y += item._y;
-        count++;
-    });
-
-    return [total_x / count, total_y / count];
+function calculate_centers() {
+    for (let index = 0; index < hidden_images.length; index++) {
+        var img = hidden_images[index];
+        var landmarks = images_landmarks[index]
+        var total_x = 0;
+        var total_y = 0;
+        var count = 0;
+        landmarks._positions.forEach(function(item, index) {
+            total_x += item._x;
+            total_y += item._y;
+            count++;
+        });
+        f_c = [(total_x / count)/img.width, (total_y / count)/img.height];
+        images_centers.push(f_c);
+    }
+    console.log(images_landmarks);
+    console.log(images_centers);
 }
-
-console.log(calculateAverage(arry));
 
 function circle() {
     let circles = images_div.querySelectorAll("img");
@@ -52,9 +61,7 @@ function circle() {
     for (let i = 0; i < circles.length; ++i) {
         let circle = circles[i];
         let angle = dangle * i;
-        circle.style.transform = `translate(${Math.cos(angle) * r}px, ${
-      Math.sin(angle) * r
-    }px)`;
+        circle.style.transform = `translate(${Math.cos(angle) * r}px, ${Math.sin(angle) * r}px)`;
         circle.style.opacity = `${op}`;
     }
 }
@@ -63,8 +70,11 @@ function center() {
     let circles = images_div.querySelectorAll("img");
     let op = 1 / circles.length;
     for (let i = 0; i < circles.length; ++i) {
-        let circle = circles[i];
-        circle.style.transform = "scale(2)";
-        circle.style.opacity = `${op}`;
+        try {
+            circles[i].style.transform = `scale(1) translate(${-images_centers[i][0]*circles[i].width}px, ${-images_centers[i][1]*circles[i].height}px)`;
+        } catch (error) {
+            circles[i].style.transform = `scale(1)`;            
+        }
+        circles[i].style.opacity = `${op}`;
     }
 }
