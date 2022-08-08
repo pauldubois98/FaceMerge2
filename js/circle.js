2dac30c0514a705ebfc279e69edec68fd3e06638
pauldubois98 <pauldubois98@gmail.com>
2022-08-08 22:35:54 +0200

    center the centers

--- js/circle.js
+++ js/circle.js
@@ -15,15 +15,15 @@ function addImage(file) {
 }
 
 function upload(event) {
-    for (let index = 0; index < event.target.files.length; index++) {
-        var file = event.target.files[index];
+    for (let i = 0; i < event.target.files.length; i++) {
+        var file = event.target.files[i];
         addImage(file);
     }
     circle();
 }
 async function calculate_landmarks() {
-    for (let index = 0; index < hidden_images.length; index++) {
-        var img = hidden_images[index];
+    for (let i = 0; i < hidden_images.length; i++) {
+        var img = hidden_images[i];
         landmarks = await get_landmarks(img);
         images_landmarks.push(landmarks);
     }
@@ -33,22 +33,35 @@ async function calculate_landmarks() {
 }
 
 function calculate_centers() {
-    for (let index = 0; index < hidden_images.length; index++) {
-        var img = hidden_images[index];
-        var landmarks = images_landmarks[index]
+    images_centers = [];
+    let circles = images_div.querySelectorAll("img");
+    for (let i = 0; i < hidden_images.length; i++) {
+        var img = hidden_images[i];
+        var landmarks = images_landmarks[i]
         var total_x = 0;
         var total_y = 0;
         var count = 0;
-        landmarks._positions.forEach(function(item, index) {
+        landmarks._positions.forEach(function(item, i) {
             total_x += item._x;
             total_y += item._y;
             count++;
         });
-        f_c = [(total_x / count)/img.width, (total_y / count)/img.height];
+        f_c = [circles[i].width*(total_x / count)/img.width, circles[i].height*(total_y / count)/img.height];
         images_centers.push(f_c);
     }
-    console.log(images_landmarks);
-    console.log(images_centers);
+    var c_x = 0;
+    var c_y = 0;
+    for (let i = 0; i < images_centers.length; i++) {
+        c_x += images_centers[i][0];
+        c_y += images_centers[i][1];
+    }
+    c_x /= images_centers.length
+    c_y /= images_centers.length
+    for (let i = 0; i < images_centers.length; i++) {
+        images_centers[i][0] -= c_x;
+        images_centers[i][1] -= c_y;
+    }
+    console.log(c_x, c_y)
 }
 
 function circle() {
@@ -69,7 +82,7 @@ function center() {
     let op = 1 / circles.length;
     for (let i = 0; i < circles.length; ++i) {
         try {
-            circles[i].style.transform = `scale(1) translate(${-images_centers[i][0]*circles[i].width}px, ${-images_centers[i][1]*circles[i].height}px)`;
+            circles[i].style.transform = `scale(1) translate(${-images_centers[i][0]}px, ${-images_centers[i][1]}px)`;
         } catch (error) {
             circles[i].style.transform = `scale(1)`;            
         }
